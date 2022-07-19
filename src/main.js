5c0ee67cb4c89cc1666a54aac7aada51ba4607b2
Ryan <morenoryan33@gmail.com>
2022-07-19 04:28:43 -0400

    final commit with finshed code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5000";
 
-function bulkDelete(ids) {
-  
-}
+function bulkDelete(ids) { // I put all promises within the promise.all function
+  return Promise.all(ids.map((id) => {
+    const URL = `${BASE_URL}/${id}`
+    return axios.delete(URL) //I mapped out all of the delete request
+  })).then((response) => {
+    return Promise.resolve(ids.map((id) => { // I than returned a resolved promise remapping all of the ids
+      return {id: id}
+    }))
+    }
+   )
+  }
+    
+    
+      
+    
+    
+    
+    
+
 
 module.exports = {
   bulkDelete,
